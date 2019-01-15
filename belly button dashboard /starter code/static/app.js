d626def2d091a6ad2f35ace17ea64b9fea5fb8e1
DDiaz07 <40971229+DDiaz07@users.noreply.github.com>
2019-01-14 22:00:48 -0500

    Add files via upload

---
+++ belly button dashboard /starter code/static/app.js
@@ -0,0 +1,169 @@
+function buildMetadata(sample) {
+
+  // @TODO: Complete the following function that builds the metadata panel
+  console.log("building metadata")
+  // Use `d3.json` to fetch the sample data for the plots
+  d3.json("/metadata/"+sample).then((data) => {
+      console.log(data);
+  // Use `d3.json` to fetch the metadata for a sample
+
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var PANEL = document.getElementById("sample-metadata");
+    // Clear any existing metadata
+    PANEL.innerHTML = '';
+    // Loop through all of the keys in the json response and
+    // create new metadata tags
+    for(var key in data) {
+        h6tag = document.createElement("h6");
+        h6Text = document.createTextNode(`${key}: ${data[key]}`);
+        h6tag.append(h6Text);
+        PANEL.appendChild(h6tag);
+    }
+
+    // Use `.html("") to clear any existing metadata
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+
+    // BONUS: Build the Gauge Chart
+    buildGauge(data.WFREQ);
+  });  //closing function
+}
+
+function buildGauge(wfreq) {
+        // Enter the washing frequency between 0 and 180
+        var level = wfreq*20;
+        // Trig to calc meter point
+        var degrees = 180 - level,
+            radius = .5;
+        var radians = degrees * Math.PI / 180;
+        var x = radius * Math.cos(radians);
+        var y = radius * Math.sin(radians);
+        // Path: may have to change to create a better triangle
+        var mainPath = 'M -.0 -0.05 L .0 0.05 L ',
+            pathX = String(x),
+            space = ' ',
+            pathY = String(y),
+            pathEnd = ' Z';
+        var path = mainPath.concat(pathX,space,pathY,pathEnd);
+        var data = [{ type: 'scatter',
+        x: [0], y:[0],
+            marker: {size: 12, color:'850000'},
+            showlegend: false,
+            name: 'Freq',
+            text: level,
+            hoverinfo: 'text+name'},
+        { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+        rotation: 90,
+        text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+        textinfo: 'text',
+        textposition:'inside',
+        marker: {
+            colors:[
+                'rgba(0, 105, 11, .5)', 'rgba(10, 120, 22, .5)',
+                'rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                'rgba(240, 230, 215, .5)', 'rgba(255, 255, 255, 0)']},
+        labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+        hoverinfo: 'label',
+        hole: .5,
+        type: 'pie',
+        showlegend: false
+        }];
+        var layout = {
+        shapes:[{
+            type: 'path',
+            path: path,
+            fillcolor: '850000',
+            line: {
+                color: '850000'
+            }
+            }],
+        title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week',
+        height: 500,
+        width: 500,
+        xaxis: {zeroline:false, showticklabels:false,
+                    showgrid: false, range: [-1, 1]},
+        yaxis: {zeroline:false, showticklabels:false,
+                    showgrid: false, range: [-1, 1]}
+        };
+        var GAUGE = document.getElementById('gauge');
+        Plotly.newPlot(GAUGE, data, layout);
+}
+
+function buildCharts(sample) {
+  console.log("building charts")
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  d3.json("/samples/"+sample).then((data) => {
+      console.log(data); //this is pulling the data from the api end point /samples which is in the "app.py"
+
+      // @TODO: Build a Bubble Chart using the sample data
+      var bubbleLayout = {
+        margin: { t: 0 },
+        hovermode: 'closest',
+        xaxis: { title: 'OTU ID' }
+      };
+      var bubbleData = [{
+          x: data['otu_ids'],
+          y: data['sample_values'],
+          text: data['otu_labels'],
+          mode: 'markers',
+          marker: {
+              size: data['sample_values'],
+              color: data['otu_ids'],
+              colorscale: "Earth",
+          }
+      }];
+      var BUBBLE = document.getElementById('bubble');
+      Plotly.plot(BUBBLE, bubbleData, bubbleLayout);
+
+      // @TODO: Build a Pie Chart
+      // HINT: You will need to use slice() to grab the top 10 sample_values,
+      // otu_ids, and labels (10 each).
+      console.log(data['sample_values'].slice(0, 10))
+      var pieData = [{
+          values: data['sample_values'].slice(0, 10),
+          labels: data['otu_ids'].slice(0, 10),
+          hovertext: data['otu_labels'].slice(0, 10),
+          hoverinfo: 'hovertext',
+          type: 'pie'
+      }];
+      var pieLayout = {
+          margin: { t: 0, l: 0 }
+      };
+      var PIE = document.getElementById('pie');
+      Plotly.plot(PIE, pieData, pieLayout);
+
+    });
+}
+
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init();
